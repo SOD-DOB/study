472c1736d1c63b2547e38457aede012564da0994
SOD-DOB <47728686+SOD-DOB@users.noreply.github.com>
2019-05-31 09:41:46 +0800

    Update crawl_pool.py

--- crawl_pool.py
+++ crawl_pool.py
@@ -36,11 +36,11 @@ UA = [
     'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0',
 ]
 
-proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
-    "host": 'http-pro.abuyun.com',
-    "port": '9010',
-    "user": 'H6B8WY3O4988764P',
-    "pass": 'B44BC8FAD71691A4',
+proxyMeta = "" % {
+    "host": '',
+    "port": '',
+    "user": '',
+    "pass": '',
 }
 proxies = {'https': proxyMeta,
            'http': proxyMeta, }
@@ -58,21 +58,21 @@ header = {
 
 # finewest
 sql1 = pymysql.connect (
-    host='rm-2zehi2ns25kee8568o.mysql.rds.aliyuncs.com',
-    port=3306,
-    user='fwread',
-    password='Finewest1127',
-    db='finewest'
+    host='',
+    port=,
+    user='',
+    password='',
+    db=''
 )
 cursor1 = sql1.cursor ()
 
 # finewest_test
 sql2 = pymysql.connect (
-    host='rm-2zehi2ns25kee8568o.mysql.rds.aliyuncs.com',
-    port=3306,
-    user='fwtest',
-    password='Finewest2018',
-    db='fwtest'
+    host='',
+    port=,
+    user='',
+    password='',
+    db=''
 )
 cursor2 = sql2.cursor ()
 
